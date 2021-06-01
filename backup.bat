333cde4ff98b64d43fef0c3ab2159b8ee760df67
Joakim Olsson <barbatron@gmail.com>
2021-06-01 04:25:56 +0200

    misc improvements, description

--- backup.bat
+++ backup.bat
@@ -1,14 +1,30 @@
 @echo off
 
-rem local date time in predictable/orderable format yyyy-MM-dd
+if not defined noita_root set noita_root=%userprofile%\AppData\LocalLow\Nolla_Games_Noita
+if not defined noita_desc (
+  if not "%1"=="-" ( 
+    set noita_desc=%*
+    echo Using description "%noita_desc%"
+  )
+)
+
+setlocal
+set noita_backup_root=%noita_root%\backups
+set noita_save_dir=%noita_root%\save00
+
+rem local date time in predictable/orderable format yyyyMMdd_HHmm - not globally safe but ok
 rem https://stackoverflow.com/questions/203090/how-do-i-get-current-date-time-on-the-windows-command-line-in-a-suitable-format
 for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
-set ldt=%ldt:~0,4%-%ldt:~4,2%-%ldt:~6,2%
+set noita_backup_dir=save00-%ldt:~0,4%%ldt:~4,2%%ldt:~6,2%-%ldt:~8,2%%ldt:~10,2%%ldt:~12,2%
 
-set noita_backup_root=%userprofile%\AppData\LocalLow\Nolla_Games_Noita\backups
-set noita_last_backup=%noita_backup_root%\%ldt%-%RANDOM%
+rem ### Allow passing description, e.g.
+if not "%noita_desc%"=="" (
+  set noita_backup_dir=%noita_backup_dir%-%noita_desc%
+)
 
-echo Backing up to %noita_last_backup%...
+set noita_last_backup=%noita_backup_root%\%noita_backup_dir%
+
+echo Backing up to "%noita_last_backup%"...
 
 if exist %noita_last_backup% (
   echo Deleting existing...
@@ -16,6 +32,6 @@ if exist %noita_last_backup% (
 )
 
 echo Copying files...
-xcopy %userprofile%\AppData\LocalLow\Nolla_Games_Noita\save00\*.* %noita_last_backup%\*.* /e /y /i /q
+xcopy "%noita_save_dir%\*.*" "%noita_last_backup%" /e /y /i /q
 
 echo OK
--- restore.bat
+++ restore.bat
@@ -1,87 +1,84 @@
 @echo off
 
 if not defined noita_root set noita_root=%userprofile%\AppData\LocalLow\Nolla_Games_Noita
-
 set noita_backup_root=%noita_root%\backups
-set noita_game_folder=%noita_root%\save00
+set noita_save_dir=%noita_root%\save00
 
-set backupdir=
+set noita_backup_dir=
 set prompt_user=y
 
-if "%1" == "auto" (
-  rem ### Auto mode
+if "%1" == "" (
+  rem ### Default mode w/ prompt
+  call :find_latest
+) else if "%1" == "auto" (
+  rem ### Auto mode - restore latest created
   echo Auto mode
   set prompt_user=n
   call :find_latest
-) else if "%1"=="" (
-  rem ### Default mode w/ prompt
+) else if "%1" == "ls" (
+  rem ### List backups
+  dir %noita_backup_root% /b /od
   call :find_latest
-) else if not "%1"=="" (
-  rem ### Some argument that is not "auto" = name of backup directory under %noita_backup_root%
-  set backupdir=%1
+  exit /b
+) else if not "%1" == "" (
+  rem ### Some argument not yet handled - assumed to be name of backup directory under %noita_backup_root%
+  set noita_backup_dir=%1
 )
 
 rem ### Verify and produce full path
-if not defined backupdir (
+if not defined noita_backup_dir (
   echo No backup selected - aborting.
-  goto end
+  goto :end
 )
-if not exist %noita_backup_root%\%backupdir% (
-  echo Backup "%backupdir%" not found under %noita_backup_root% - aborting.
-  goto end
+if not exist %noita_backup_root%\%noita_backup_dir% (
+  echo Backup "%noita_backup_dir%" not found under %noita_backup_root% - aborting.
+  goto :end
 )
 
-set noita_last_backup_attempt=%noita_backup_root%\%backupdir%
+set noita_last_backup_attempt=%noita_backup_root%\%noita_backup_dir%
 
-if "%prompt_user%" NEQ "n" (
+if not "%prompt_user%" == "n" (
   call :do_prompt %noita_last_backup_attempt%
-  if errorlevel 1 ( 
+  if errorlevel 1 (
     echo Aborted by user...  
-    call :cleanup
-    exit /b -1
+    goto :end
   )
 )
 
-rem ### Keep any existing game
-if exist %noita_game_folder% call :backup_current
+rem ### Keep any existing game, for safety
+if exist %noita_save_dir% call :backup_current
 
 rem ### Restore backup files
-if not exist %noita_game_folder% mkdir %noita_game_folder%
+if not exist %noita_save_dir% mkdir %noita_save_dir%
 echo Copying files...
-xcopy %noita_last_backup_attempt%\*.* %noita_game_folder% /e /y /i /q
+xcopy "%noita_last_backup_attempt%\*.*" "%noita_save_dir%" /e /y /i /q
 
 rem ### Restore complete
-echo Restored "%backupdir%" OK at %DATE% %TIME%
-
-:cleanup
-set backupdir=
-set prompt_user=y
-exit /b
-
-:end
-call :cleanup
-exit /b
+echo Restored "%noita_backup_dir%" OK at %DATE% %TIME%
 
 :find_latest
-echo Finding most recent backup...
-set backupdir=
-for /f "tokens=*" %%a in ('dir %noita_backup_root%\2* /b /od') do set backupdir=%%a
-if "%backupdir%"=="" (
+set noita_backup_dir=
+for /f "tokens=*" %%a in ('dir %noita_backup_root% /b /od') do set "noita_backup_dir=%%a"
+if "%noita_backup_dir%" == "" (
   echo No backups found!
-  exit /b 1
+  exit /b 2
 )
+echo Latest is: %noita_backup_dir%
 exit /b
 
 :backup_current
 echo Copying existing save00 to save00.old... 
-if exist %noita_game_folder%.old rmdir %noita_game_folder%.old /s /q
-mkdir %noita_game_folder%.old
-xcopy %noita_game_folder%\*.* %noita_game_folder%.old /e /y /i /q
+if exist %noita_save_dir%.old rmdir %noita_save_dir%.old /s /q
+mkdir %noita_save_dir%.old
+xcopy %noita_save_dir%\*.* %noita_save_dir%.old /e /y /i /q
 echo Current game backup OK
 exit /b
 
 :do_prompt
 set noita_restore_ok=n
-set /P noita_restore_ok=Restore save from %1 (y/[n])? 
-if /I "%noita_restore_ok%" NEQ "y" exit /B 1
+set /P noita_restore_ok=Restore backup "%1"? (y/[n]) 
+if "%noita_restore_ok%" == "n" exit /b 1
+exit /b
+
+:end
 exit /b
