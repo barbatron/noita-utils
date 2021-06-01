7ac40a6121a22be2acf9784a30c3c63a4c334c5e
Joakim Olsson <barbatron@gmail.com>
2021-06-01 04:27:04 +0200

    cleanup

--- backup.bat
+++ backup.bat
@@ -17,7 +17,6 @@ rem https://stackoverflow.com/questions/203090/how-do-i-get-current-date-time-on
 for /F "usebackq tokens=1,2 delims==" %%i in (`wmic os get LocalDateTime /VALUE 2^>NUL`) do if '.%%i.'=='.LocalDateTime.' set ldt=%%j
 set noita_backup_dir=save00-%ldt:~0,4%%ldt:~4,2%%ldt:~6,2%-%ldt:~8,2%%ldt:~10,2%%ldt:~12,2%
 
-rem ### Allow passing description, e.g.
 if not "%noita_desc%"=="" (
   set noita_backup_dir=%noita_backup_dir%-%noita_desc%
 )
